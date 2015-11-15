8db27bb1ea8cfdfacdcc0731b2ce12d5bbf7c04a
seral13 <ser_al13@hotmail.com>
2015-11-15 16:58:50 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,8 +1,8 @@
 # This script runs the functions
-# 'makeMatrix' and 'cacheSolve'
+# 'makeCacheMatrix' and 'cacheSolve'
 
 source("cachematrix.R")
 x <- rbind(c(2, -1/8), c(-1/8, 2))
-m <- makeMatrix(x)
+m <- makeCacheMatrix(x)
 m$get()
 cacheSolve(m)
